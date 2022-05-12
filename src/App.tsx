7adfe1f17478ff98bf884620850f83846b7e4284
Fábio Bellaver <fabio.bellaver658@gmail.com>
2022-05-12 20:22:33 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+import { SideBar } from './components/SideBar';
+import { Content } from './components/Content';
+
+import './styles/global.scss';
+import './styles/sidebar.scss';
+import './styles/content.scss';
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+export function App() {
+
+  const [selectedGenreId, setSelectedGenreId] = useState(1);
+  const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>({} as GenreResponseProps);
+
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'row' }}>
+      <SideBar
+        selectedGenreId={selectedGenreId}
+        handleClickButton={handleClickButton}
+      />
+      <Content
+        selectedGenre={selectedGenre}
+        selectedGenreId={selectedGenreId}
+      />
+    </div>
+  )
+}
